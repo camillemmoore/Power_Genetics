ffe010b306eef1e06269f646b412ea422da5e74a
Unknown <camille.moore@gmail.com>
2018-04-04 11:11:16 -0600

    Update non-centrality parameter search
    Corrected DF from 1 to 2 in the search function.  This function is used in the ss and detectable OR calculations for 2df tests.

--- genpwr/R/non_centrality_search_function.R
+++ genpwr/R/non_centrality_search_function.R
@@ -7,16 +7,16 @@
 #'
 #' This function is set to 0 and solved for x, the non-centrality parameter to determine the sample size in \code{\link{ss.calc}}
 #'
-#' @param x the non-centrality parameter 
+#' @param x the non-centrality parameter
 #' @param power the desired power
 #' @param stat the detectable likelihood ratio test statistic
-#' @param Alpha the desired type 1 error rate 
+#' @param Alpha the desired type 1 error rate
 #' @param df the degrees of freedom for the likelihood ratio test
-#' 
+#'
 #' @return numeric value of the function
 #'
 #' @export
 #'
 ncp.search<-
   function(x, power, stat, Alpha, df){
-  (1-power)-pchisq(qchisq(1-Alpha, df, ncp=0), df=1, ncp = x) }
+  (1-power)-pchisq(qchisq(1-Alpha, df, ncp=0), df=2, ncp = x) }
