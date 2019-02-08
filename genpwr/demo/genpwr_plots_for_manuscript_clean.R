2a3c68fcef950096c195cbff3955a4d5580f7d11
Sean <jacobsons@njhealth.org>
2019-02-08 15:22:14 -0700

    cleaning up the demo

--- genpwr/demo/genpwr_plots_for_manuscript_clean.R
+++ genpwr/demo/genpwr_plots_for_manuscript_clean.R
@@ -9,7 +9,10 @@ library(genpwr)
 
 # Calculate power to detect and OR of 1.5 for a GWAS with N=5,000 and N=20,000
 # Assume 1 control per case and 5 x 10^-8 significance level
-pw <- power.calc(N=c(5000, 20000), Case.Rate = 0.5, OR=1.5, Alpha=0.00000005, MAF=seq(0.01, 0.8, 0.005))
+pw <- genpwr.calc(calc = "pow", model = "logistic", ge.interaction = NULL,
+  N=c(5000, 20000), Case.Rate = 0.5, OR=1.5, Alpha=0.00000005, MAF=seq(0.01, 0.8, 0.005))
+x <- "RAF"
+
 
 pw$Power <- pw$`Power_at_Alpha_5e-08`
 pw$Test.Model <- factor(pw$Test.Model, levels = c('Dominant', "Additive", "Recessive", "2df"))
@@ -20,33 +23,33 @@ panel.by <- "True.Model"
 plot_obj_5000 <- ggplot(data = pw[pw$N_total==5000,], aes(x = pw[pw$N_total==5000, x], 
                                           y = Power, group = Test.Model, colour = Test.Model)) + 
   geom_line() + 
-  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~temp2[, panel.by])
+  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~pw[pw$N_total==5000,][, panel.by])
 
 
 plot_obj_20000 <- ggplot(data = pw[pw$N_total==20000,], aes(x = pw[pw$N_total==20000, x], 
                                            y = Power, group = Test.Model, colour = Test.Model)) + 
   geom_line() + 
-  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~temp2[, panel.by])
+  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~pw[pw$N_total==20000,][, panel.by])
 
 plot_obj_5000 + ggtitle("N = 5,000") 
 
 plot_obj_20000 + ggtitle("N = 20,000")
 
 
-pdf('/Users/mooreca/Documents/genpwr_plots_OR_1.25_alpha_genome.pdf', height = 3, width=9)
+pdf('genpwr_plots_OR_1.25_alpha_genome.pdf', height = 3, width=9)
 
-plot_obj_2000 + ggtitle("N=2,000")
-plot_obj_3000 + ggtitle("N=3,000")
-plot_obj_4000 + ggtitle("N=4,000")
+# plot_obj_2000 + ggtitle("N=2,000")
+# plot_obj_3000 + ggtitle("N=3,000")
+# plot_obj_4000 + ggtitle("N=4,000")
 plot_obj_5000 + ggtitle("N=5,000")
 
-plot_obj_10000 + ggtitle("N=10,000")
-plot_obj_10000 + xlim(c(0,0.15))+ ggtitle("N=10,000, Zoom 1")
-plot_obj_10000 + xlim(c(0,0.15)) + ylim(c(0.7,1))+ ggtitle("N=10,000, Zoom 2")
+# plot_obj_10000 + ggtitle("N=10,000")
+# plot_obj_10000 + xlim(c(0,0.15))+ ggtitle("N=10,000, Zoom 1")
+# plot_obj_10000 + xlim(c(0,0.15)) + ylim(c(0.7,1))+ ggtitle("N=10,000, Zoom 2")
 
-plot_obj_15000+ ggtitle("N=15,000")
-plot_obj_15000 + xlim(c(0,0.1))+ ggtitle("N=15,000, Zoom 1")
-plot_obj_15000 + xlim(c(0,0.1))+ ylim(c(0.7,1))+ ggtitle("N=15,000, Zoom 2")
+# plot_obj_15000+ ggtitle("N=15,000")
+# plot_obj_15000 + xlim(c(0,0.1))+ ggtitle("N=15,000, Zoom 1")
+# plot_obj_15000 + xlim(c(0,0.1))+ ylim(c(0.7,1))+ ggtitle("N=15,000, Zoom 2")
 
 plot_obj_20000+ ggtitle("N=20,000")
 plot_obj_20000 + xlim(c(0,0.1))+ ggtitle("N=20,000, Zoom 1")
@@ -65,7 +68,7 @@ Ncase <- 3624
 Ncontrol <- 4442
 Ntotal <- Ncase+Ncontrol
 cr <- Ncase/Ntotal
-p_e <- (0.72*Ncase + 0.63*Ncontrol)/Ntotal
+p_e <- p_ever <- (0.72*Ncase + 0.63*Ncontrol)/Ntotal
 
 ################################################
 # Calculate power for a range of scenarios:
@@ -73,7 +76,8 @@ p_e <- (0.72*Ncase + 0.63*Ncontrol)/Ntotal
 # OR_GE = 1.5, 1.75, 2
 ################################################
 # OR_G = 5
-pw_5 <- power_envir.calc(N = Ntotal, Case.Rate = cr, MAF = seq(0.05, 0.8, 0.05),
+pw_5 <- genpwr.calc(calc = "pow", model = "logistic", ge.interaction = "binary", 
+                           N = Ntotal, Case.Rate = cr, MAF = seq(0.05, 0.8, 0.05),
                            OR_G = c(5), OR_E = 1.6, OR_GE = c(1.5,1.75,2), P_e = p_e,
                            Alpha = 0.00005, True.Model = "All", Test.Model = "All")
 pw_5$RAF <- pw_5$MAF
@@ -88,21 +92,22 @@ x <- 'RAF'
 plot_obj_5_1.5 <- ggplot(data = pw_5[pw_5$OR_GE==1.5,], aes(x = pw_5[pw_5$OR_GE==1.5,x], 
                                              y = Power, group = Test.Model, colour = Test.Model)) + 
   geom_line() + 
-  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~temp2[, panel.by])
+  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~pw_5[pw_5$OR_GE==1.5,][, panel.by])
 
 plot_obj_5_1.75 <- ggplot(data = pw_5[pw_5$OR_GE==1.75,], aes(x = pw_5[pw_5$OR_GE==1.75,x], 
                                               y = Power, group = Test.Model, colour = Test.Model)) + 
   geom_line() + 
-  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~temp2[, panel.by])
+  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~pw_5[pw_5$OR_GE==1.75,][, panel.by])
 
 plot_obj_5_2 <- ggplot(data = pw_5[pw_5$OR_GE==2,], aes(x =pw_5[pw_5$OR_GE==2,x], 
                                            y = Power, group = Test.Model, colour = Test.Model)) + 
   geom_line() + 
-  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~temp2[, panel.by])
+  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~pw_5[pw_5$OR_GE==2,][, panel.by])
 
 
 # OR_G = 1.5
-pw_1.5 <- power_envir.calc(N = Ntotal, Case.Rate = cr, MAF = seq(0.05, 0.8, 0.05),
+pw_1.5 <- genpwr.calc(calc = "pow", model = "logistic", ge.interaction = "binary", 
+                            N = Ntotal, Case.Rate = cr, MAF = seq(0.05, 0.8, 0.05),
                             OR_G = c(1.5), OR_E = 1.6, OR_GE = c(1.5,1.75,2), P_e = p_ever,
                             Alpha = 0.00005, True.Model = "All", Test.Model = "All")
 pw_1.5$RAF <- pw_1.5$MAF
@@ -114,21 +119,22 @@ pw_1.5$Test.Model <- factor(pw_1.5$Test.Model, levels = c('Dominant', 'Additive'
 plot_obj_1.5_1.5 <- ggplot(data = pw_1.5[pw_1.5$OR_GE==1.5,], aes(x = pw_1.5[pw_1.5$OR_GE==1.5,x], 
                                               y = Power, group = Test.Model, colour = Test.Model)) + 
   geom_line() + 
-  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~temp2[, panel.by])
+  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~pw_1.5[pw_1.5$OR_GE==1.5,][, panel.by])
 
 plot_obj_1.5_1.75 <- ggplot(data = pw_1.5[pw_1.5$OR_GE==1.75,], aes(x = pw_1.5[pw_1.5$OR_GE==1.75,x], 
                                                y = Power, group = Test.Model, colour = Test.Model)) + 
   geom_line() + 
-  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~temp2[, panel.by])
+  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~pw_1.5[pw_1.5$OR_GE==1.75,][, panel.by])
 
 plot_obj_1.5_2 <- ggplot(data = pw_1.5[pw_1.5$OR_GE==2,], aes(x = pw_1.5[pw_1.5$OR_GE==2,x], 
                                             y = Power, group = Test.Model, colour = Test.Model)) + 
   geom_line() + 
-  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~temp2[, panel.by])
+  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~pw_1.5[pw_1.5$OR_GE==2,][, panel.by])
 
 
 # OR_G = 1.25
-pw_1.25 <- power_envir.calc(N = Ntotal, Case.Rate = cr, MAF = seq(0.05, 0.8, 0.05),
+pw_1.25 <- genpwr.calc(calc = "pow", model = "logistic", ge.interaction = "binary", 
+                           N = Ntotal, Case.Rate = cr, MAF = seq(0.05, 0.8, 0.05),
                            OR_G = c(1.25), OR_E = 1.6, OR_GE = c(1.5,1.75,2), P_e = p_ever,
                            Alpha = 0.00005, True.Model = "All", Test.Model = "All")
 pw_1.25$RAF <- pw_1.25$MAF
@@ -140,17 +146,17 @@ pw_1.25 <-pw_1.25[order(pw_1.25$True.Model),]
 plot_obj_1.25_1.5 <- ggplot(data = pw_1.25[pw_1.25$OR_GE==1.5,], aes(x = pw_1.25[pw_1.25$OR_GE==1.5,x], 
                                                y = Power, group = Test.Model, colour = Test.Model)) + 
   geom_line() + 
-  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~temp2[, panel.by])
+  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~pw_1.25[pw_1.25$OR_GE==1.5,][, panel.by])
 
 plot_obj_1.25_1.75 <- ggplot(data = pw_1.25[pw_1.25$OR_GE==1.75,], aes(x = pw_1.25[pw_1.25$OR_GE==1.75,x], 
                                               y = Power, group = Test.Model, colour = Test.Model)) + 
   geom_line() + 
-  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~temp2[, panel.by])
+  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~pw_1.25[pw_1.25$OR_GE==1.75,][, panel.by])
 
 plot_obj_1.25_2 <- ggplot(data = pw_1.25[pw_1.25$OR_GE==2,], aes(x = pw_1.25[pw_1.25$OR_GE==2,x], 
                                                y = Power, group = Test.Model, colour = Test.Model)) + 
   geom_line() + 
-  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~temp2[, panel.by])
+  xlab(x) + geom_hline(aes(yintercept=0.8), linetype="dashed",color='black')+ facet_wrap(~pw_1.25[pw_1.25$OR_GE==2,][, panel.by])
 
 #################################
 # Make Plots
